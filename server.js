131bddec939b54340d91a01399465c20810ecf27
Leta Keane <letakeane+turing@gmail.com>
2019-02-04 23:14:40 -0700

    Require https

--- server.js
+++ server.js
@@ -2,6 +2,15 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
+const requireHTTPS = (request, response, next) => {
+  if (request.header('x-forwarded-proto') != 'https') {
+    return response.redirect(`https://${request.header('host')}${request.url}`);
+  }
+  next();
+};
+
+if (process.env.NODE_ENV === 'production') { app.use(requireHTTPS); }
+
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'pw8ball';
 
