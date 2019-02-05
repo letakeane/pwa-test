9c450334011f7cefe0f808f1b04ecbceee8ef018
Leta Keane <letakeane+turing@gmail.com>
2019-02-04 23:19:51 -0700

    Add service worker

---
+++ public/service-worker.js
@@ -0,0 +1,20 @@
+this.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('assets-v1').then(cache => {
+      return cache.addAll([
+        '/',
+        '/server.js',
+        '/index.css',
+        '/lib/shake.js'
+      ])
+    })
+  );
+});
+
+this.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
